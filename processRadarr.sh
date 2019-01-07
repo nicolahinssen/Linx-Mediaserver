e0f27fa69744e05299ff54b7c917e18f166dd7ec
Nicola Hinssen <nicola.hinssen@gmail.com>
2019-01-07 19:28:52 +0100

    update

---
+++ processRadarr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### Radarr variables ###
+
+export radarr_movie_id="$radarr_movie_id"
+export radarr_movie_title="$radarr_movie_title"
+export radarr_movie_path="$radarr_movie_path"
+export radarr_movie_imdbid="$radarr_movie_imdbid"
+export radarr_moviefile_id="$radarr_moviefile_id"
+export radarr_moviefile_relativepath="$radarr_moviefile_relativepath"
+export radarr_moviefile_path="$radarr_moviefile_path"
+export radarr_moviefile_quality="$radarr_moviefile_quality"
+export radarr_moviefile_qualityversion="$radarr_moviefile_qualityversion"
+export radarr_moviefile_releasegroup="$radarr_moviefile_releasegroup"
+export radarr_moviefile_scenename="$radarr_moviefile_scenename"
+export radarr_moviefile_sourcepath="$radarr_moviefile_sourcepath"
+export radarr_moviefile_sourcefolder="$radarr_moviefile_sourcefolder"
+export radarr_download_id="$radarr_download_id"
+
+export -p | grep 'radarr_*' > /srv/scripts/radarr_variables.txt
+
+cd "$(dirname "$radarr_moviefile_path")"
+
+#subliminal --addic7ed escapereality tessel --opensubtitles escape-reality tessel download -l nl -l en  -p addic7ed -p opensubtitles -p podnapisi -p shooter -p thesubdb -p tvsubtitles -r metadata -f "$radarr_moviefile_path"
+
+/srv/scripts/sickbeard_mp4_automator/venv/bin/python /srv/scripts/sickbeard_mp4_automator/postRadarr.py 
---
+++ processSonarr.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+### Sonarr variables ###
+
+export sonarr_series_id="$sonarr_series_id"
+export sonarr_series_title="$sonarr_series_title"
+export sonarr_series_path="$sonarr_series_path"
+export sonarr_series_tvdbid="$sonarr_series_tvdbid"
+export sonarr_series_tvmazeid="$sonarr_series_tvmazeid"
+export sonarr_series_imdb="$sonarr_series_imdb"
+export sonarr_series_type="$sonarr_series_type"
+export sonarr_episodefile_id="$sonarr_episodefile_id"
+export sonarr_episodefile_relativepath="$sonarr_episodefile_relativepath"
+export sonarr_episodefile_path="$sonarr_episodefile_path"
+export sonarr_episodefile_episodecount="$sonarr_episodefile_episodecount"
+export sonarr_episodefile_seasonnumber="$sonarr_episodefile_seasonnumber"
+export sonarr_episodefile_episodenumbers="$sonarr_episodefile_episodenumbers"
+export sonarr_episodefile_episodeairdates="$sonarr_episodefile_episodeairdates"
+export sonarr_episodefile_episodeairdatesutc="$sonarr_episodefile_episodeairdatesutc"
+export sonarr_episodefile_episodetitles="$sonarr_episodefile_episodetitles"
+export sonarr_episodefile_quality="$sonarr_episodefile_quality"
+export sonarr_episodefile_qualityversion="$sonarr_episodefile_qualityversion"
+export sonarr_episodefile_releasegroup="$sonarr_episodefile_releasegroup"
+export sonarr_episodefile_scenename="$sonarr_episodefile_scenename"
+export sonarr_episodefile_sourcepath="$sonarr_episodefile_sourcepath"
+export sonarr_episodefile_sourcefolder="$sonarr_episodefile_sourcefolder"
+export sonarr_deletedrelativepaths="$sonarr_deletedrelativepaths"
+export sonarr_deletedpaths="$sonarr_deletedpaths"
+export sonarr_download_id="$sonarr_download_id"
+
+export -p | grep 'sonarr_*' > /srv/scripts/sonarr_variables.txt
+
+### Find subtitles ###
+#
+#cd "$(dirname "$sonarr_episodefile_path")"
+#
+#subliminal --addic7ed escapereality tessel --opensubtitles escape-reality tessel download -l nl -l en  -p addic7ed -p opensubtitles -p podnapisi -p shooter -p thesubdb -p tvsubtitles -r metadata -f "$sonarr_episodefile_path"
+
+/srv/scripts/sickbeard_mp4_automator/venv/bin/python /srv/scripts/sickbeard_mp4_automator/postSonarr.py 
